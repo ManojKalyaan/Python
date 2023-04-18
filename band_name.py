93fe184bad6086f8b6d5b47a416602cc6f7d1eca
Manoj Kalyaan <kalyaanmanoj@gamil.com>
2023-04-18 16:54:45 +0530

    Band Name

---
+++ band_name.py
@@ -0,0 +1,4 @@
+print ("welcome to the Band Name Generator")
+city = input ("What's the name of the city you grew up in?\n")
+pet = input ("What's your pet name in childhood\n")
+print("Name of the band can be " + city + " " + pet)
